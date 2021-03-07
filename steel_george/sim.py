9960192b0da1400d3e372d3a7f29807ffda01efe
lexa-orl <75783766+lexa-orl@users.noreply.github.com>
2021-03-07 17:41:26 +0300

    Update sim.py

--- steel_george/sim.py
+++ steel_george/sim.py
@@ -4,6 +4,7 @@ import sys
 import time
 from random import randint
 
+
 class Player:  #описываем персонажа
     """docstring for Player;."""
     hp = 50       # Heal point
@@ -11,7 +12,7 @@ class Player:  #описываем персонажа
     pw = 5        #Power
     level = 0     #уровень
     money = 5        # money
-    xp = 0        # Очки опыта  experience
+    xp = 0       # Очки опыта  experience
     max_xp = 10  #
     medcine = 5
     heal_hp = 8   #Сила аптечки
@@ -20,21 +21,37 @@ class Player:  #описываем персонажа
 p = Player()
 
 #Настройки
-def Settings(p):
-    print("1. To save game")
-    print("2. To load game")
-    print("3. To delete save")
-    print("4.")
-    print("5.")
-    print("6.")
-    print("7.")
+
+#Таверно
+def taverna(p):
+    os.system('cls||clear')
+    print("""
+    Ты вошел в таверну.
+1. Подойти к барной стойке.
+2. Пойти поиграть в очко.
+3. Уйти
+    """)
+    k = input("")
+    if k == "1":
+        os.system('cls||clear')
+    if k == "2":
+        os.system('cls||clear')
+        import BlackJack
+    if k == "3":
+        os.system('cls||clear')
+        menu_simple(p)
+    os.system('cls||clear')
+
+
+
 
 #инвентарь
 def inventary(p):
     os.system('cls||clear')
-    print("You have {} medcine".format(p.medcine))
-    print("Your wearphone: {}".format(p.wearphone))
-    print("1. Go back.")
+    print("--Инвентарь--")
+    print("У тебя {} Аптечек".format(p.medcine))
+    print("Тво оружие: {}".format(p.wearphone))
+    print("1. Назад.")
     n = input("")
     if n == "1":
         os.system('cls||clear')
@@ -43,36 +60,44 @@ def inventary(p):
 
 def menu_upgrade(p):
     while p.money > 0:
-        print("Choose your upgrades! Money: {}$".format(p.money))
-        print("-----")
-        print("1. HP {}/{}  10$".format(p.hp, p.max_hp))
-        print("2. Power {}  15$".format(p.pw))
-        print("3. Go to main menu")
-        n = input("Number:")
+        print("Выбирай прокачку!             Баланс: {}$".format(p.money))
+        print("----------------------------------------------")
+        print("1. Здоровье {}/{}  20$".format(p.hp, p.max_hp))
+        print("2. Сила {}  35$".format(p.pw))
+        print("3. Вернуться")
+        n = input("Номер:")
         if n =="1":
-            p.hp +=5
-            p.money -=10
-            p.max_xp +=5
+            if p.money >= 20:
+                p.hp +=5
+                p.money -=20
+                p.max_xp +=5
+            else: print("Нехватает денег")
+            time.sleep(3)
             os.system('cls||clear')
         if n == "2":
-            p.pw +=2
-            p.money -=15  #меню прокачки
+            if p.money >= 35:
+                p.pw +=2
+                p.money -=35  #меню прокачки
+            else: print("Нехватает денег")
+            time.sleep(3)
             os.system('cls||clear')
         if n == "3":
             break
-            os.system('cls||clear')
+        os.system('cls||clear')
 #магазин
 def menu_market(p):
     while p.money > 0:
-        print("You have {}$ ".format(p.money))
-        print("Welcome to market")
-        print("1. medcine  1$")
-        print("2. Wooden stick (+3 heat) 5$")
-        print("3. Cheep sword (+5 heat) 10$")
-        print("4. Usual sword (+9 heat) 40$")
-        print("5. Legendary sword(+15 heat) 90$")
-        print("6. Go back")
-        n = input("Number:")
+        print("У тебя {}$ ".format(p.money))
+        print("""
+            Добро пожаловать в магазин!
+    1. Аптечка  1$
+    2. Деревянная палка (+3 heat) 5$
+    3. Дешевый меч (+5 heat) 10$
+    4. Обычный меч (+9 heat) 40$
+    5. Легендарный меч(+15 heat) 90$
+    6. Вернуться
+""")
+        n = input("Номер:")
 
         if n == "1":
             p.medcine +=1
@@ -83,7 +108,7 @@ def menu_market(p):
                 p.money -=5
                 p.wearphone = "Wooden stick"
                 p.wearphone_damage =  3
-            else: print("You haven't enough money")
+            else: print("Нехватает денег")
             time.sleep(3)
             os.system('cls||clear')
         if n == "3":
@@ -91,7 +116,7 @@ def menu_market(p):
                 p.money -=10
                 p.wearphone = "Cheep sword"
                 p.wearphone_damage =  5
-            else: print("You haven't enough money")
+            else: print("Нехватает денег")
             time.sleep(3)
             os.system('cls||clear')
         if n == "4":
@@ -99,7 +124,7 @@ def menu_market(p):
                 p.money -=40
                 p.wearphone = "Usual sword"
                 p.wearphone_damage =  9
-            else: print("You haven't enough money")
+            else: print("Нехватает денег")
             time.sleep(3)
             os.system('cls||clear')
         if n == "5":
@@ -107,7 +132,7 @@ def menu_market(p):
                 p.money -=90
                 p.wearphone = "Legendary sword"
                 p.wearphone_damage =  15
-            else: print("You haven't enough money")
+            else: print("Нехватает денег")
             time.sleep(3)
             os.system('cls||clear')
         if n == "6":
@@ -115,20 +140,20 @@ def menu_market(p):
         os.system('cls||clear')
 
 def menu_stats(p):
-    print("Player stats")
-    print("-----")
-    print("HP {}/{}".format(p.hp, p.max_hp))
-    print("Power {}".format(p.pw))
-    print("Healing {}".format(p.heal_hp))
-    print("Level {}".format(p.level))
-    print("experience {}".format(p.xp))
-    print("skillpoint {}".format(p.sp))
-    input("Enter to continue.")
+    print("  Статус игрока")
+    print("----------------------")
+    print("Здоровье {}/{}".format(p.hp, p.max_hp))
+    print("Сила {}".format(p.pw))
+    print("Лечение {}".format(p.heal_hp))
+    print("Уровень {}".format(p.level))
+    print("Опыт {}".format(p.xp))
+    print("Баланс {}$".format(p.money))
+    input("Enter чтобы продолжить.")
     os.system('cls||clear')   #Cтатистика персонажа
 
 def Game_Ower():
     os.system('cls||clear')
-    print("Game Ower")
+    print("Конец игры")
     time.sleep(3)
     sys.exit()   #Конец игры (с завершением работы)
 
@@ -136,16 +161,20 @@ def Game_Ower():
 
 #Стартовое меню (главное)
 def menu_simple(p):
+    os.system('cls||clear')
     while True:
-        print("Choose what to do")
-        print("1. Go Fight")
-        print("2. Check your stats")
-        print("3. To Upgrade")
-        print("4. Market")
-        print("5. Inventary")
-        print("6. Settings")
+        print("""
+    Выбери действие...
+1. Пойти сражаться
+2. Посмотреть статистику персонажа
+3. Прокачать навыки
+4. Пойти в магазин
+5. Пойти в таверну
+6. Инвентарь
+7. Настройки
+""")
 
-        n = input("Number:")
+        n = input("Номер:")
         if n == "1":
             os.system('cls||clear')
             menu_fight(p)
@@ -160,36 +189,39 @@ def menu_simple(p):
             menu_market(p)
         if n == "5":
             os.system('cls||clear')
-            inventary(p)
+            taverna(p)
         if n == "6":
             os.system('cls||clear')
+            inventary(p)
+        if n == "7":
+            os.system('cls||clear')
             Settings(p)
-
+    os.system('cls||clear')
 
 def menu_fight(p):
     os.system('cls||clear')             #Бой с противником
     ehp = 2* randint(4,20)
     epw = 3* randint(1,5)
     while ehp>0:
-        print("Enemy: {}  Power:{}".format(ehp, epw))
-        print("You: {} of {}, Power: {}".format(p.hp, p.max_hp, p.pw))
+        print("Здоровье врага: {}  Сила врага:{}".format(ehp, epw))
+        print("Твое здоровье: {} c {}, Твоя сила: {}".format(p.hp, p.max_hp, p.pw))
         print("~~~~~~" )
-        print("1. Punch with power {}".format(p.pw))
-        print("2. Medcine {} Heal (+{}) ".format(p.medcine, p.heal_hp))
-        print("3. Run away")
-        n = input("Number:")
+        print("1. Ударить с силой {}".format(p.pw + p.wearphone_damage))
+        print("2. У тебя {} аптечек которые востанавливают по {}ед. здоровья ".format(p.medcine, p.heal_hp))
+        print("3. Сбежать")
+        n = input("Номер:")
         os.system('cls||clear')
 
         if n == "1":
             r=randint(1,2)
             if r == 1:
                 ehp -= (p.pw + p.wearphone_damage)
-                print("you hit the enemy")
+                print("Ты наносишь удар")
             if r == 2:
                 p.hp -= epw
-                print("enemy hit you")
+                print("Твой враг наносит удар")
                 if p.hp <= 0:
-                    print("ypu loose")
+                    print("Ты помер")
                     Game_Ower()
 
         if n == "2":
@@ -198,16 +230,16 @@ def menu_fight(p):
                 if p.hp > p.max_hp:
                     p.hp = p.max_hp
                     p.medcine -= 1
-                    print("Healing...{}".format(p.hp))
-            else: print("You haven't medcine")
+                    print("Лечение...{}".format(p.hp))
+            else: print("Аптечки кончились!")
 
         if n == "3":
             r = randint(1,3)
             if r == 3:
-                print("You ran away")
+                print("Ты сбежал")
                 return True
             else:
-                print("you can't run")
+                print("Ты не смог сбежать")
 
     os.system('cls||clear')
     p.xp += epw
@@ -226,6 +258,7 @@ def menu_fight(p):
 ########################
 
 os.system('cls||clear')
+import loading
 
 
 def logo():
@@ -241,32 +274,17 @@ def logo():
     █▒═▒█
     █▒═▒█
     █▒═▒█
-    █▒═▒█  █▀▄ █░█ ▄▀▀ █░░     █░▄▀ █▀▀ ▀▄░▄▀     ▀█▀ ▄▀▄     ▄▀▀ ▀█▀ ▄▀▄ █▀▀▄ ▀█▀
-    █▒═▒█  █░█ █░█ ░▀▄ █▀▄     █▀▄░ █▀▀ ░░█░░     ░█░ █░█     ░▀▄ ░█░ █▀█ █▐█▀ ░█░
-    █▒═▒█  █▀░ ░▀░ ▀▀░ ▀░▀     ▀░▀▀ ▀▀▀ ░░▀░░     ░▀░ ░▀░     ▀▀░ ░▀░ ▀░▀ ▀░▀▀ ░▀░
+    █▒═▒█
+    █▒═▒█
+    █▒═▒█
     █▒═▒█
     █▒═▒█
     █▒═▒█
      █▒█
       █
 """)
+os.system('cls||clear')
 logo()
 time.sleep(3)
-print("Loading.")
-time.sleep(0.5)
-os.system('cls||clear')
-print("Loading..")
-time.sleep(0.5)
-os.system('cls||clear')
-print("Loading...")
-time.sleep(0.5)
-os.system('cls||clear')
-print("Loading.")
-time.sleep(0.5)
-os.system('cls||clear')
-print("Loading..")
-time.sleep(0.5)
-os.system('cls||clear')
-
 menu_simple(p)
 Game_Ower()
